8fed528ceaaef35c60f8d7cf232beb44a824adc6
Breiart <aversanamichele@gmail.com>
2025-07-01 13:18:21 +0200

    Modified manticore do_solidity script

--- tools/manticore-0.3.7/scripts/do_solidity.sh
+++ tools/manticore-0.3.7/scripts/do_solidity.sh
@@ -13,7 +13,21 @@ for c in `python3 "$BIN/printContractNames.py" "${FILENAME}"`; do
         if [ -n "$ARGS" ]; then
             manticore --no-colors --contract "${c}" "${FILENAME#/}" $ARGS
         else
-            manticore --no-colors --contract "${c}" "${FILENAME#/}"
+            #manticore --no-colors --contract "${c}" "${FILENAME#/}"
+            manticore \
+                --no-colors \
+                --contract "${c}" "${FILENAME#/}"\
+                --txlimit 1 \
+                --evm.ignore_balance EVM.IGNORE_BALANCE \
+                --evm.oog ignore \
+                --evm.defaultgas 1000000 \
+                --smt.timeout 10 \
+                --smt.memory 512 \
+                --core.timeout 120 \
+                --core.procs 1 \
+                --avoid-constant \
+                --limit-loops \
+                
         fi
         mv /mcore_* /results
 done
