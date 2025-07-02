861e0fadd970acaadce95f708a790eadbf9b2b3d
Breiart <aversanamichele@gmail.com>
2025-07-02 17:28:54 +0200

    Added files for osiris, semgrep, sfuzz and solhint

--- tools/osiris/scripts/do_solidity.sh
+++ tools/osiris/scripts/do_solidity.sh
@@ -4,7 +4,6 @@ FILENAME="$1"
 TIMEOUT="$2"
 BIN="$3"
 MAIN="$4"
-ARGS="${5:-}"
 
 export PATH="$BIN:$PATH"
 chmod +x "$BIN/solc"
@@ -16,7 +15,7 @@ CONTRACTS=$(python3 "$BIN"/printContractNames.py "$FILENAME")
 OPT_CONTRACT=""
 if [ "$MAIN" -eq 1 ]; then
     if (echo "$CONTRACTS" | grep -q "$CONTRACT"); then
-        OPT_CONTRACT="--target-contracts $CONTRACT"
+        OPT_CONTRACT="--contract $CONTRACT"
     else
         echo "Contract '$CONTRACT' not found in $FILENAME"
         exit 127
@@ -26,10 +25,9 @@ fi
 OPT_TIMEOUT=""
 if [ "$TIMEOUT" -gt 0 ]; then
     # TO = TIMEOUT * 80%
-    # the remaining 20% are for Oyente to finish
+    # the remaining 20% are for osiris to finish
     TO=$(( (TIMEOUT*8+9)/10 ))
     OPT_TIMEOUT="-glt $TO"
 fi
 
-cd /oyente
-/oyente/oyente/oyente.py $OPT_TIMEOUT -s "$FILENAME" $OPT_CONTRACT $ARGS
+python osiris/osiris.py $OPT_TIMEOUT -s "$FILENAME"  $OPT_CONTRACT
